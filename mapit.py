4e8d4af2875a8f367603010d329a3c3d7d3d924f
t-ohba <the-arctic-fox@live.jp>
2019-05-08 23:03:34 +0900

    webbrowserモジュール

--- mapit.py
+++ mapit.py
@@ -6,8 +6,8 @@ import sys
 import pyperclip
 
 if len(sys.argv) > 1:
-    address = ' '.join(sys.argv[1:])
+    address = ' '.join(sys.argv[1:]) # コマンドラインから住所を取得する
 else:
-    address = pyperclip.paste()
+    address = pyperclip.paste() # クリップボードから住所を取得する
 
 webbrowser.open('https://google.com/maps/place/' + address)
